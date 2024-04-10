bebf491d1e8d5d3fe4bb732f623132c4de731154
Grokhen <152092079+Grokhen@users.noreply.github.com>
2024-04-10 19:11:02 +0000

    excercise done

--- src/models.py
+++ src/models.py
@@ -7,26 +7,44 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Favorite_planet(Base):
+    __tablename__ = 'favorite_planet'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    favorite_planet = Column(String(250), ForeignKey('planets.id'))
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+class Favorite_character(Base):
+    __tablename__ = 'favorite_character'
+    id = Column(Integer, primary_key=True)
+    favorite_character = Column(String(250), ForeignKey('character.id'))
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+class Planets(Base):
+    __tablename__ = 'planets'
+    id = Column(Integer, primary_key=True)
+    planet_name = Column(String(250), )
+    planet_population = Column(String(250))
+    planet_rotation = Column(String(125))
+    user = relationship(Favorite_planet)
+
+class Character(Base):
+    __tablename__ = 'character'
+    id = Column(Integer, primary_key=True)
+    character_name = Column(String(125))
+    character_age = Column(String(100))
+    character_weight = Column(String(10))
+    user = relationship(Favorite_character)
 
     def to_dict(self):
         return {}
 
-## Draw from SQLAlchemy base
+
 render_er(Base, 'diagram.png')
